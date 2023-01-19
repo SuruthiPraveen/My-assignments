fff0709a367d6f0faf116709444d253e992d73da
SuruthiPraveen <121445843+SuruthiPraveen@users.noreply.github.com>
2023-01-19 19:33:51 +0530

    Add files via upload

---
+++ day2assignment/org/system/Computer.java
@@ -0,0 +1,15 @@
+package org.system;
+
+public class Computer {
+
+	public void computerModel() {
+
+System.out.println("Compuer Model is HP");
+	}
+	
+public static void main(String[] args) {
+	Computer obj= new Computer();
+	obj.computerModel();
+}
+
+}
